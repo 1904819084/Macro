0abce390a4fd333860284d66527d2bc69eb81a3f
林翔 <linxiang.2027@bytedance.com>
2026-06-16 14:51:13 +0800

    batch 5: OpenTimer bin/lib/config and small downloads

---
+++ ReMaP/thirdparty/OpenTimer/ot/config.hpp
@@ -0,0 +1,8 @@
+#define OT_VERSION_MAJOR 2
+#define OT_VERSION_MINOR 1
+#define OT_VERSION_PATCH 0
+#define OT_VERSION "2.1.0"
+#define OT_HOME "/Users/bytedance/Documents/trae_projects/Remap/DAC25-ReMaP/ReMaP/thirdparty/OpenTimer"
+#define OT_HOST_SYSTEM "Darwin-24.6.0"
+#define OT_INSTALL_PREFIX "/Users/bytedance/Documents/trae_projects/Remap/DAC25-ReMaP/ReMaP/install"
+#define OT_TCLSH_PATH "/usr/bin/tclsh"
